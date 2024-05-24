ce9872db35da2e40917c780b0e7ec833e01c2a6d
glaydson-alves <glaydson.alves36@gmail.com>
2024-05-24 20:43:42 -0300

    tipos de dados

---
+++ src/TiposDeVariaveis.java
@@ -0,0 +1,16 @@
+public class TiposDeVariaveis {
+    
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+        long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F;
+        double salario = 1275.33;
+        
+        System.out.println(idade);
+
+        // Observe que o tipo long precisa terminar com L, o tipo float precisa terminar com F e alguns cenários do dia-a-dia, podem estimular uma alteração de tipos de dados convencional.
+    }
+    
+}

2667455731ff56d469f80f73f7c999e7fed4e563
glaydson-alves <glaydson.alves36@gmail.com>
2024-05-30 20:23:02 -0300

    update for each

--- estruturas-condicionais/src/ExemploFor.java
+++ estruturas-condicionais/src/ExemploFor.java
@@ -39,13 +39,14 @@ public class ExemploFor {
     public static void vetorDois(String[] args) {
         String alunos [] =  {"FELIPE","JONAS","JULIA","MARCOS"};
         
-            //Forma abreviada
-        for(String aluno : alunos) {
-          System.out.println(alunos);
+        //Forma abreviada
+        for (String aluno : alunos) {
+          System.out.println(aluno);
         }
+
         /*
         String aluno : alunos -> De forma abreviada, é criada uma variável nome obtendo um elemento do vetor a cada ocorrência;
         A impressão de cada aluno é realizada.
-         */
+        */
     }
 }
